746cb164b0a0d9cb739c6d989850c0ca8f2d01e8
Marcelo Henrique de Sousa Pinheiro <marcelo.pinheiro@edu.udesc.br>
2022-06-10 23:54:08 -0300

    verbose from update litaAleatoria

--- listaAleatoria.py
+++ listaAleatoria.py
@@ -2,6 +2,6 @@ from random import *
 
 def listaAleatoria():
     lista_aleatoria = []
-    for i in range(0, 10000000, 1):
+    for i in range(0, 1000, 1):
         lista_aleatoria.append(randint(-10000, 10000))
     return lista_aleatoria
